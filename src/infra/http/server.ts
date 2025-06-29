a1455594220661ecaa4216df365507ce91626e60
Andre Cantarino <cantarino.andre@gmail.com>
2025-06-29 12:53:09 -0300

    feat: added swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,35 +1,58 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
-import { hasZodFastifySchemaValidationErrors, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { env } from "@/env";
-import { uploadImageRoute } from "./routes/upload-image";
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env'
+import { uploadImageRoute } from '@/infra/http/routes/upload-image'
 
-const server = fastify();
+const server = fastify()
 
-server.setValidatorCompiler(validatorCompiler);
-server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
 server.setErrorHandler((error, request, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
-    reply.status(400).send({
+    return reply.status(400).send({
       message: 'Validation error',
       issues: error.validation,
-    });
+    })
   }
 
-  // Enviar error para observabilidade
-  console.error(error);
-  reply.status(500).send({
-    message: 'Internal server error',
-    error: error.message,
-  });
-});
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/DataDog/Grafana/OTel)
 
-server.register(fastifyCors, { origin: "*" });
-server.register(uploadImageRoute);
+  console.error(error)
 
-console.log(env.DATABASE_URL);
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
-server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
-  console.log("Server is running on port 3333");
-});
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
+
+console.log(env.DATABASE_URL)
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP Server running!')
+})
